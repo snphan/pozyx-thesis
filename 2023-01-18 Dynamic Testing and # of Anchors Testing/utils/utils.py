e6e2e228d24598c6774f30e28823c6f7d16b7ac7
Steven Phan <snphan@ualberta.ca>
2023-01-24 14:08:14 -0700

    functionalize cleaning steps

---
+++ 2023-01-18 Dynamic Testing and # of Anchors Testing/utils/utils.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+import numpy as np
+from scipy import interpolate
+
+#MARK: PREPROCESSING
+def extract_time_labels(fp):
+    """
+    Expects the file to have lines that read: 
+    LABEL_NAME: TIMESTAMP
+
+    Eg. Go to fridge: 16900000.104
+    """
+    with open(fp) as f:
+        labels = f.readlines()
+
+    label_keys = ["Timestamp", "Label"]
+    labels = [label.replace("\n","").split(": ")[::-1] for label in labels]
+    labels =  [{label_keys[0]: label[0], label_keys[1]:label[1]} for label in labels]
+    return labels
+
+def copy_df_format(df):
+    """
+    Returns an empty df with the corresponding df
+    """
+    copy_df = pd.DataFrame(columns=df.columns)
+    copy_df.index.name = df.index.name
+    return copy_df
+
+#MARK: CLEANING DATA
+
+def remove_periods(df, periods):
+    """Deletes the selected periods"""
+    for period in periods:
+        mask = (df.index > period[0]) & (df.index < period[1])
+        df = df.drop(df.index[mask])
+    return df
+
+def df_iterp1d(df, num_points):
+
+    interp_df = pd.DataFrame()
+    interp_df.index.name = df.index.name
+
+    x = df.index.values
+    xnew = np.linspace(x[0], x[-1], num=num_points)
+
+    for col in df.columns:
+        col_data = df[col].values
+        f = interpolate.interp1d(x, col_data)
+        new_col_data = f(xnew)
+        new_df = pd.DataFrame({'Timestamp': xnew, col: new_col_data}).set_index('Timestamp')
+        interp_df = pd.concat([interp_df, new_df], axis=1)
+
+    return interp_df
+
+def interp1d_periods(df, periods, num_points):
+    for period in periods:
+        mask = (df.index > period[0]) & (df.index < period[1])
+        interp_df = df_iterp1d(df[mask], num_points)
+        df = df.drop(df.index[mask])
+        df = pd.concat([df, interp_df])
+    return df
+
+#MARK: VISUALIZATION
+
+def plot_pozyx_data_with_timings(data: pd.DataFrame, columns: list[str], labels: list[dict], title="Data with Timings", ylim=(-1000, 15000)):
+    """
+    Args
+    ----
+
+    labels: list of dictionary {'Timestamp': <TIMESTAMP>, 'Label': <LABEL_NAME>} 
+    """
+    ax = data.loc[:, columns].plot(figsize=(20,10))
+    ax.set_title(title)
+    ax.set_ylim(ylim)
+    ax.set_ylabel("Position (mm)")
+    for label in labels:
+        if label["Label"] != "10 sec elapsed":
+            ax.axvline(float(label['Timestamp']), color="red")
+            ax.text(float(label['Timestamp'])+2,0,label["Label"], rotation=90, color="red", size="large")
+        else:
+            ax.axvline(float(label['Timestamp']), color="black")
+    return ax
+
+
+def plot_interactive_pozyx_data(data, title, xlim=12000, ylim=12000):
+    @widgets.interact(to_index=(0, 2000),window=(1,100))
+    def f(to_index=0,window=1):
+        fig = plt.figure(figsize=(10,10))
+        data_clean = data.rolling(window).mean()
+        plt.plot(data_clean.iloc[:to_index, 0], data_clean.iloc[:to_index, 1])
+        plt.xlabel("X(mm)")
+        plt.ylabel("Y(mm)")
+        plt.title(title)
+        plt.xlim(0, xlim)
+        plt.ylim(0,ylim)
