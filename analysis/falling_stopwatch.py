a98de906ed7eb83e2e477da95fdcbf65f03407b4
Steven Phan <snphan@ualberta.ca>
2023-02-07 16:22:05 -0700

    modularize and add z analysis

---
+++ analysis/falling_stopwatch.py
@@ -0,0 +1,38 @@
+import time
+
+TRANSITIONS = [
+    'Go Hallway between kitchen and living',
+    '10 sec elapsed',
+    'Fall',
+    '10 sec elapsed',
+    'Living Room',
+    '10 sec elapsed',
+    'Fall',
+    '10 sec elapsed',
+    'bedroom',
+    '10 sec elapsed',
+    'Fall',
+    '10 sec elapsed',
+    'bathroom',
+    '10 sec elapsed',
+    'Fall',
+    '10 sec elapsed',
+    'kitchen',
+    '10 sec elapsed',
+    'Fall',
+    '10 sec elapsed',
+    'hallway between kitchen and living',
+    '10 sec elapsed',
+]
+
+if __name__ == "__main__":
+    start_time = None
+    counter = 0
+    trial = input("Trial Number: ")
+    anchor_num = input("Number of Anchors: ")
+    with open(f"FALLING_A{anchor_num}_{trial}.txt", "w+") as f:
+        while counter < len(TRANSITIONS):
+            input("")
+            f.write(f"{TRANSITIONS[counter]}: {time.time()}\n")
+            print(f"{TRANSITIONS[counter]}: {time.time()}")
+            counter += 1
