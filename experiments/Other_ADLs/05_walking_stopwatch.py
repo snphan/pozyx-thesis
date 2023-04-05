805fce1ab833575282f603a39bff6e2e2a8286b4
HaydenGowing <97248426+HaydenGowing@users.noreply.github.com>
2023-04-05 13:43:51 -0600

    Added/Modified Mop and walk stopwatch scripts

--- experiments/Other_ADLs/05_walking_stopwatch.py
+++ experiments/Other_ADLs/05_walking_stopwatch.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 TRANSITIONS = [
     'quiet standing (living entrance)', # 5 seconds
-    'mop', 
+    'walk', 
     'quiet standing (living entrance)', # 5 seconds
     '5 seconds elapsed',
 ]
 
 if __name__ == "__main__":
-    data_dir = Path(__file__).resolve().parent.joinpath('data', 'MOP')
+    data_dir = Path(__file__).resolve().parent.joinpath('data', 'WALK')
     data_dir.mkdir(parents=True, exist_ok=True)
     start_time = None
     counter = 0
     trial = input("Trial Number: ")
     anchor_num = input("Number of Anchors: ")
     participant = input("Input Participant Initials: ")
-    with open(data_dir.joinpath(f"MOP_{participant}_A{anchor_num}_{trial}.txt"), "w+") as f:
+    with open(data_dir.joinpath(f"WALK_{participant}_A{anchor_num}_{trial}.txt"), "w+") as f:
         while counter < len(TRANSITIONS):
             input("")
             f.write(f"{TRANSITIONS[counter]}: {time.time()}\n")
